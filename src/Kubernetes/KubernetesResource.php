dc553b6b40dd40747db1c8e5f80e2e849efb03e9
Angel <angel.campos.m@outlook.com>
2022-10-10 09:19:24 +0200

    feat: created KubernetesResource.php

---
+++ src/Kubernetes/KubernetesResource.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace Acamposm\IacGenerator\Kubernetes;
+
+use stdClass;
+
+abstract class KubernetesResource
+{
+    protected array $annotations = [];
+    protected array $labels = [];
+    protected string $name;
+    protected string $namespace = '';
+    protected array $ports = [];
+    protected array $selectors = [];
+
+    /**
+     * KubernetesResource constructor.
+     *
+     * @param string $kind
+     * @param string $apiVersion
+     */
+    public function __construct(
+        public string $kind,
+        public string $apiVersion,
+    )
+    { }
+
+    public function name(string $name): KubernetesResource
+    {
+        $this->name = $name;
+        return $this;
+    }
+
+    public function namespace(string $namespace): KubernetesResource
+    {
+        $this->namespace = $namespace;
+        return $this;
+    }
+
+    public function addAnnotation(string $key, string $value): KubernetesResource
+    {
+        $this->annotations[$key] = $value;
+        return $this;
+    }
+
+    public function addAnnotations(array $annotationPairs): KubernetesResource
+    {
+        foreach ($annotationPairs as $key => $value) {
+            $this->annotations[$key] = $value;
+        }
+        return $this;
+    }
+
+    public function addLabel(string $key, string $value): KubernetesResource
+    {
+        $this->labels[$key] = $value;
+        return $this;
+    }
+
+    public function addLabels(array $labelPairs): KubernetesResource
+    {
+        foreach ($labelPairs as $key => $value) {
+            $this->labels[$key] = $value;
+        }
+        return $this;
+    }
+    
+    public function addPort(
+        int $exposedPort,
+        int $targetPort,
+        string|null $name = null,
+        string $protocol = 'TCP'
+    ): KubernetesResource
+    {
+        $port = [
+            'port' => $exposedPort,
+            'protocol' => $protocol,
+            'targetPort' => $targetPort,
+        ];
+
+        if ($name !== null) {
+            $port['name'] = $name;
+        }
+
+        $this->ports[] = $port;
+        return $this;
+    }
+}
